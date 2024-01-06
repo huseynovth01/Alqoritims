4876d88afbc0ac5ffc02a29b04032bc1f6bd6939
mac book pro <macbookpro@macs-MacBook-Pro.local>
2024-01-06 22:18:04 +0400

    start

---
+++ js/main.js
@@ -0,0 +1,53 @@
+//* ededin factorialinin hesablanmasi
+
+// function factorial(x){
+// if(x===0){
+//   return 1;
+// }
+// return x*factorial(x-1);
+// }
+// console.log(factorial(3));
+// console.log ("ededin factorialinin hesablanmasi");
+
+//*--------------------------
+
+
+//* iki müsbət ədədin ən böyük ümumi bölənini (GCD) tapmaq
+
+// var gcd = function(x, y){
+//   if (! y){
+//     return x ;
+//   }
+//   return gcd (y,x %y);
+// };
+// console.log(gcd(124,342));
+// console.log ("iki müsbət ədədin ən böyük ümumi bölənini");
+
+//*--------------------------
+
+
+
+//* aralıqdakı tam ədədləri goster
+
+// var range = function (start_number, end_number){
+//   if(end_number-start_number ===2){
+//     return [start_number+1];
+//   }
+//   else {
+//     var list = range (start_number,end_number-1);
+//     list.push(end_number-1);
+//     return list;
+//   }
+// }
+// console.log(range(12,18));
+// console.log ("araaralıqdakı tam ədədlər ededler");
+
+//*--------------------------
+
+
+
+//* 
+
+
+
+//*--------------------------
